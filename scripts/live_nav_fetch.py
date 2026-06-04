58e722efa984c357f186d9d19f6f7c40d578b962
Bindu Chaudhary <bin654321du@gmail.com>
2026-06-03 20:02:16 -0700

    Day 1: Data ingestion complete

---
+++ scripts/live_nav_fetch.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+
+funds = {
+    "HDFC_Top_100": 125497,
+    "SBI_Bluechip": 119551,
+    "ICICI_Bluechip": 120503,
+    "Nippon_Large_Cap": 118632,
+    "Axis_Bluechip": 119092,
+    "Kotak_Bluechip": 120841
+}
+
+for fund_name, amfi_code in funds.items():
+
+    url = f"https://api.mfapi.in/mf/{amfi_code}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        data = response.json()
+
+        nav_df = pd.DataFrame(data["data"])
+
+        nav_df.to_csv(
+            f"../data/raw/{fund_name}_live_nav.csv",
+            index=False
+        )
+
+        print(f"Saved: {fund_name}")
+
+    else:
+        print(f"Failed: {fund_name}")
